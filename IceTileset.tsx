bd4fe84b7b6e56a9a918ee8bbecf895ca3296759
metalmichi <77055605+metalmichi@users.noreply.github.com>
2021-01-06 14:48:40 +0100

    k
    jk

---
+++ IceTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="IceTileset" tilewidth="32" tileheight="32" tilecount="180" columns="20">
+ <image source="../../../Downloads/IceTileset.png" width="640" height="288"/>
+</tileset>
